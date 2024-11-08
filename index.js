95182d7cb4862a175bb5732ed98742bebf4dcb59
Malgorzata Staszewska-Bui <136642341+staszewska@users.noreply.github.com>
2024-11-08 13:47:58 +0100

    authentication logic

--- index.js
+++ index.js
@@ -55,3 +55,44 @@ app.post("/users", async (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+// Allow user to log in
+
+const jwtSecret = "your_jwt_secret";
+
+const jwt = require("jsonwebtoken"),
+  passport = require("passport");
+
+require("./passport");
+
+let generateJWTToken = (user) => {
+  return jwt.sign(user, jwtSecret, {
+    subject: user.Email, // This is the email you’re encoding in the JWT
+    expiresIn: "7d", // This specifies that the token will expire in 7 days
+    algorithm: "HS256", // This is the algorithm used to “sign” or encode the values of the JWT
+  });
+};
+
+/* POST login. */
+module.exports = (router) => {
+  router.post("/login", (req, res) => {
+    //function from passport that authenticates a user.
+    passport.authenticate("local", { session: false }, (error, user, info) => {
+      if (error || !user) {
+        return res.status(400).json({
+          message: "Something is not right",
+          user: user,
+        });
+      }
+
+      //function that "logs in" the user by attaching the user's data to the request object
+      req.login(user, { session: false }, (error) => {
+        if (error) {
+          res.send(error);
+        }
+        let token = generateJWTToken(user.toJSON());
+        return res.json({ user, token });
+      });
+    })(req, res);
+  });
+};
